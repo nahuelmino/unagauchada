b7e07fff55221f4ad5dc441d11f5b05e5cd294f1
Sabri <sabriferrari9@gmail.com>
2017-06-22 18:59:40 -0300

    limitacion de la longitud de los campos del formulario para comprar creditos

--- resources/views/creditos/comprar.blade.php
+++ resources/views/creditos/comprar.blade.php
@@ -9,30 +9,30 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Comprar créditos</div>
+                <div class="panel-heading" align="center"> <h4>Comprar créditos</h4></div>
 
                 <div class="panel-body">
                     <form action="/compras" method="post" class="form">
                         {{ csrf_field() }}
                         <div class="form-group">
                             <label for="nombre_completo">Nombre completo:</label>
-                            <input id="nombre_completo" name="nombre_completo" class="form-control" type="text" required>
+                            <input id="nombre_completo" name="nombre_completo" class="form-control" type="text" maxlength="42" placeholder="Ingrese su nombre y apellidos completos" required>
                         </div>
                         <div class="form-group">
                             <label for="compra_creditos">Número de tarjeta:</label>
-                            <input id="compra_creditos" type="text" name="num_tarjeta" class="form-control numbers-only" placeholder="Ingresa los 16 numeros de tu tarjeta" required>
+                            <input id="compra_creditos" type="text" name="num_tarjeta" class="form-control numbers-only" maxlength="16" placeholder="Ingresa los 16 numeros de tu tarjeta" required>
                         </div>
                         <div class="form-group">
                             <label for="codigo_verification">Código de verificacion</label>
-                            <input id="codigo_verification" type="text" class="form-control numbers-only" name="codigo_verificacion" minlength="3" maxlength="3" required>
+                            <input id="codigo_verification" type="text" class="form-control numbers-only" name="codigo_verificacion" minlength="3" maxlength="3" placeholder="Ingrese el codigo de 3 digitos en el reverso de su tarjeta" required>
                         </div>
                         <div class="form-group">
                             <label for="fecha_inicio">Fecha inicio</label>
-                            <input id="fecha_inicio" type="text" name="fecha_inicio" class="form-control datepicker" required>
+                            <input id="fecha_inicio" type="text" name="fecha_inicio" class="form-control datepicker" placeholder="Click aqui para desplegar el calendario" required>
                         </div>
                         <div class="form-group">
                             <label for="fecha_expiracion">Fecha de expiracion</label>
-                            <input id="fecha_expiracion" type="text" name="fecha_expiracion" class="form-control datepicker" required>
+                            <input id="fecha_expiracion" type="text" name="fecha_expiracion" class="form-control datepicker" placeholder="Click aqui para desplegar el calendario" required>
                         </div>
                         <div class="form-group{{ $errors->has('0') ? ' has-error' : '' }}">
                             <button type="submit" class="btn btn-primary btn-orange">Comprar</button>
